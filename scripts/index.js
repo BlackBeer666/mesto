2b5ab247a55243d9b3c430e19385466a8d9fddf7
Alexey Galitsin <alexeygalitsin@192-168-0-111.local>
2023-01-12 23:30:45 +0300

    second interation

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const editButton = document.querySelector('.profile__editbutton');
-const closePopup = document.querySelector('.popup__closebutton');
+const closeButton = document.querySelectorAll('.popup__closebutton');
 const popupProfile = document.querySelector('#popup-profile');
 const profileName = document.querySelector('.profile__name'); 
 const profileProfession = document.querySelector('.profile__profession'); 
@@ -8,96 +8,65 @@ const nameInput = document.querySelector('.form__field_type_name');
 const professionInput = document.querySelector('.form__field_type_profession');
 
 // Функция открытия попапа
-function popupOpened(popupProfile) {
+function openPopup(popupProfile) {
   popupProfile.classList.add('popup_opened');
 }
 
 //Слушатель клика на открытие и взять данные с сайта
 editButton.addEventListener('click', () => {
-  popupOpened(popupProfile)
+  openPopup(popupProfile)
   nameInput.value = profileName.textContent; // Забираем данные с сайта
   professionInput.value = profileProfession.textContent 
 });
 
 // Функция закрытия попапа 
-function popupClose(popupProfile) {
+function closePopup(popupProfile) {
   popupProfile.classList.remove('popup_opened');
 }
-//Слушатель клика на закрытие
-closePopup.addEventListener('click', () => {
-  popupClose(popupProfile)
-});
+// Универсальный слушатель клика закрытие попапа 
+closeButton.forEach(btn => {    //forEach перебираем все элементы с кнопкой
+  const popup = btn.closest('.popup');  // ко всем родителям .popup используем функцию на закрытие
+  btn.addEventListener('click', () => closePopup(popup)); 
+}) 
+
 // Отправка данные с профиля на сайт 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileProfession.textContent = professionInput.value;
-  popupClose(popupProfile);
+  closePopup(popupProfile);
 }
 
 formProfile.addEventListener('submit', handleFormSubmit);
 
 
 // 5 cпринт 
-// Массив с автоматическими карточками  
-const initialCards = [
-  {
-    name: 'Турция',
-    link: './images/turkey.jpg'
-  },
-  {
-    name: 'Таллин',
-    link: './images/tallin.jpg'
-  },
-  {
-    name: 'Кипр',
-    link: './images/kipr.jpg'
-  },
-  {
-    name: 'Швеция',
-    link: './images/swe.jpg'
-  },
-  {
-    name: 'Испания',
-    link: './images/ispanion.jpg'
-  },
-  {
-    name: 'Карачаево-Черкесская Республика',
-    link: './images/karachaevsk.jpg'
-  }
-];  
+const cardTemplate = document.querySelector('.cards__template').content
+const cardElement = document.querySelector('.cards__element');
+const cardsContainer = document.querySelector('.cards') // выбираем секцию Cards, где будут вставлять картинки
+const formCard = document.querySelector('#form-cards');
+const popupCards = document.querySelector('#popup-cards');
+const countryInput = formCard.querySelector('#card-name');
+const urlInput = formCard.querySelector('#card-url');
+const buttonOpenPopup = document.querySelector('.profile__addbutton'); 
+const closePopupCards = document.querySelector('#close-cards');
+const popupPhoto = document.querySelector('#popup-image');
+const zoomPhoto = document.querySelector('.popup__image');
+const closePopupPhoto = document.querySelector('#close-photo');
+const description = document.querySelector('.popup__description');
 
 
-cardElements = document.querySelector('.cards__element');
-cardTemplate = document.querySelector('.cards__template').content
-cardsContainer = document.querySelector('.cards') // выбираем секцию Cards, где будут вставлять картинки
-const formCards = document.querySelector('#form-cards');
-popupCards = document.querySelector('#popup-cards');
-countryInput = formCards.querySelector('#card-name');
-urlInput = formCards.querySelector('#card-url');
-addNewCard = document.querySelector('.profile__addbutton');
-closePopupCards = document.querySelector('#close-cards');
-popupPhoto = document.querySelector('#popup-image');
-zoomPhoto = document.querySelector('.popup__image');
-closePopupPhoto = document.querySelector('#close-photo');
-description = document.querySelector('.popup__description');
-
-
-addNewCard.addEventListener('click', () => {
-  popupOpened(popupCards)
+buttonOpenPopup.addEventListener('click', () => {
+  openPopup(popupCards)
 })
-
-closePopupCards.addEventListener('click', () => {
-  popupClose(popupCards)
-})
-
 
 function addCard(card) {
   const cardItems = cardTemplate.querySelector('.cards__element').cloneNode(true); //клонируем карточку
+  const cardPhoto = cardItems.querySelector('.cards__photo');
 // Передаем значение в карточки
+  cardPhoto.src = card.link;
+  cardPhoto.alt = card.name;
   cardItems.querySelector('.cards__text').textContent = card.name;
-  cardItems.querySelector('.cards__photo').src = card.link;
-  cardItems.querySelector('.cards__photo').alt = card.name;
 
   // delete Card button
  cardItems.querySelector('.cards__trashbutton').addEventListener('click', function(deleteCard) {
@@ -110,24 +79,17 @@ cardItems.querySelector('.cards__likebutton').addEventListener('click', function
 })
 
 //увеличение картинки
-
 function zoom() {
   zoomPhoto.src = card.link;
   zoomPhoto.alt = card.name;
   description.textContent = card.name;
-  popupOpened(popupPhoto);
+  openPopup(popupPhoto);
  }
-
- cardItems.querySelector('.cards__photo').addEventListener('click', zoom);
-
- closePopupPhoto.addEventListener('click', () => {
-  popupClose(popupPhoto);
- });
+ cardPhoto.addEventListener('click', zoom);
   return cardItems;
 }
 
-
-function handleFormCardsSubmit(evt) {
+function handleFormCardSubmit(evt) {
   evt.preventDefault();
   
   const newCard = {   
@@ -137,10 +99,10 @@ function handleFormCardsSubmit(evt) {
   };
 
   cardsContainer.prepend(addCard(newCard)); // Добавляем в контейнер вначале с функцией addCard, параметр новой переменной  
-  popupClose(popupCards);
+  closePopup(popupCards); 
   evt.target.reset(); // сброс значений инпута
 }
-formCards.addEventListener('submit', handleFormCardsSubmit); // Отправка формы карточки
+formCard.addEventListener('submit', handleFormCardSubmit); // Отправка формы карточки
 
 
 // Добавление автоматических карточек
